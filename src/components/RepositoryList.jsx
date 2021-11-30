8c5faa72b400dd7c3b9421801b476a6906afa270
Saymon Damásio <saymon.damasio95@gmail.com>
2021-11-30 15:01:00 -0300

    feat: initial commit

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,14 @@
+export function RepositoryList(){
+  return <section className="repository-list">
+    <h1>Lista de repositórios</h1>
+
+    <ul>
+      <li>
+        <strong>rentx</strong>
+        <p>locadora de carros</p>
+
+        <a href="">Acessar repositorio</a>
+      </li>
+    </ul>
+  </section>
+}
