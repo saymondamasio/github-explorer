0cf27ffac39f32c05e02e50789bc08b0b52770cc
Saymon Damásio <saymon.damasio95@gmail.com>
2022-02-12 09:26:01 -0300

    refactor: update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,7 @@ module.exports = {
     isDevelopment && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
+      favicon: path.resolve(__dirname, 'public', 'favicon.png'),
     }),
   ].filter(Boolean),
   module: {
