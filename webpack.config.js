e3c9a3c1d200a758366230bae59ce39e3726ea2a
Saymon Damásio <saymon.damasio95@gmail.com>
2022-02-12 09:37:11 -0300

    refactor: update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/',
+    publicPath: isDevelopment ? '/' : 'github-explorer',
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
